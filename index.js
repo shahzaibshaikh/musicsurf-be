afc74254d06865bfccaecaaa7ac69a12dc949b06
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-28 00:03:02 +0500

    👌 server is up and running

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const axios = require('axios');
-
+const dotenv = require('dotenv');
 const app = express();
-const PORT = 3000;
+
+dotenv.config();
 
 // Replace with your own credentials
-const CLIENT_ID = 'YOUR_CLIENT_ID';
-const CLIENT_SECRET = 'YOUR_CLIENT_SECRET';
+const PORT = process.env.PORT;
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+app.get('/', (req, res) => {
+  res.send('MusicSurf Backend is up.');
+});
+
+app.listen(PORT || process.env.PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
