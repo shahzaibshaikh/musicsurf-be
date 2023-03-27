3e95cc40c064d017c1952a841a7e2f0097d39eb6
Shahzaib Shaikh <shahzaibshaikh98@gmail.com>
2023-03-27 23:02:14 +0500

    👌 removed node_modules

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3000;
+
+// Replace with your own credentials
+const CLIENT_ID = 'YOUR_CLIENT_ID';
+const CLIENT_SECRET = 'YOUR_CLIENT_SECRET';
